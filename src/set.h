bcdd330e3818cc6e1b0dbf010ef846deab53289d
RamseyK <ramsey.kant@gmail.com>
2012-10-16 11:58:41 -0700

    Added set_add, set_remove, set_difference, set_subset, set_size, and set_equals to the set implementation. Tested with driver program in algorithms repo

--- src/set.h
+++ src/set.h
@@ -1,10 +1,9 @@
-/*
- * jkcompiler
+/* 
+ * set.h
+ * From jkcompiler
  * Keilan Jackson, Ramsey Kant
- */
-
-/* set.h
  *
+ * A list based implementation of a set
  * Contains struct and function declarations for set operations
  */
 
@@ -25,10 +24,20 @@ struct set_t {
 };
 
 // Set functions
-struct set_t *new_set();
-bool set_contains(struct set_t *set, char *val);
+struct set_t *new_set(const char *val);
+void free_set(struct set_t *set);
+void set_print(struct set_t *set);
+
+int set_size(struct set_t *set);
+bool set_equals(struct set_t *set1, struct set_t *set2);
+bool set_contains(struct set_t *set, const char *val);
+
+struct set_t *set_add(struct set_t *set, const char *val);
+void set_remove(struct set_t *set, const char *val);
+
 struct set_t *set_union(struct set_t *set1, struct set_t *set2);
 struct set_t *set_intersection(struct set_t *set1, struct set_t *set2);
-void set_print(struct set_t *set);
+struct set_t *set_difference(struct set_t *set1, struct set_t *set2);
+bool set_subset(struct set_t *set1, struct set_t *set2);
 
 #endif /* SET_H_ */
